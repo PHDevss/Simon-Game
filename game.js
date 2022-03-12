4af5319cf37b875bfdb94e963e344875a3510d24
PHDevss <97169087+PHDevss@users.noreply.github.com>
2022-03-12 19:41:57 -0300

    Finalizando projeto jogo.

--- game.js
+++ game.js
@@ -1,34 +1,30 @@
 const buttonColours = ["red", "blue", "green", "yellow"]
-const gamePattern = []
-const userClickedPattern = []
+let gamePattern = []
+let userClickedPattern = []
+let level = 0
+let gameStart = false
 
 $(document).keypress(function(e){
     console.log()
-    if (e.key ==="a"){
-        startGame()
+    if (e.key ==="a" && gameStart == false){
+        gameStart = true
+        setTimeout(()=>{
+            nextSequence()
+            levelManegment()
+            watchButtonPress()
+        }, 500)
     }
 })
 
-$(".btn").click((e)=> {
-    userChosenColour = e.target.id
-    userClickedPattern.push(userChosenColour)
-    playSound(userChosenColour)
-    animatePress(userChosenColour)
-}
-)
-
-function nextSequence(){
+function nextNumber(){
     return Math.round(Math.random()*3) 
 }
 
-function startGame(){
-    const randomNumber = nextSequence()
-
+function nextSequence(){
+    const randomNumber = nextNumber()
     const randomChosenColour = buttonColours[randomNumber]
-
     gamePattern.push(randomChosenColour)
     $(`#${randomChosenColour}`).fadeOut().fadeIn(100);
-
     playSound(randomChosenColour) 
 }
 
@@ -41,4 +37,61 @@ function playSound(name) {
 function animatePress(currentColour) {
     $(`#${currentColour}`).addClass("pressed")
     setTimeout(() => $(`#${currentColour}`).removeClass("pressed"),100)
+}
+
+function watchButtonPress() {
+    $(".btn").click((e)=> {
+        userChosenColour = e.target.id
+        userClickedPattern.push(userChosenColour)
+        animatePress(userChosenColour)
+        checkAnswer(userChosenColour)
+    }
+    )
+}
+
+function levelManegment() {
+$('#level-title').html(`Level: ${level}`)
+}
+
+function checkAnswer(colorUser) {
+    let status = true
+    userClickedPattern.forEach((e,i)=>{
+        if (e !== gamePattern[i]) {
+            status = false
+        }
+    })
+    if (userClickedPattern.length == gamePattern.length){
+        if (status == true) {
+            playSound(colorUser) 
+            setTimeout(()=>{
+                userClickedPattern = []
+                level++
+                levelManegment()
+                nextSequence()
+            }, 1000)    
+        } else {
+            gameOver()  
+        } 
+    } else {
+        if (status) {
+            playSound(colorUser) 
+        } else {
+            gameOver()  
+        }
+    }
+}
+
+function gameOver() {
+    playSound('wrong')
+    level = 0
+    gamePattern = []
+    userClickedPattern = []
+    $('#level-title').html('You Lose.')
+    $('body').css("background-color","red")
+    setTimeout(()=>{
+        $(".btn").off("click")
+        $('body').css("background-color","#011F3F")
+        $('#level-title').html('Press A Key to Start Again')
+        gameStart = false
+    }, 200)
 }
